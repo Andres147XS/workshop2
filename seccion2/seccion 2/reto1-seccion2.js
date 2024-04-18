d44028b05b4264c5b92bb148341060d2209154cd
Andres147XS <tabordaandres429@gmail.com>
2024-04-17 21:42:15 -0500

    a folder is created for section 2 and challenge 1 of section 2 is started.

---
+++ seccion2/seccion 2/reto1-seccion2.js
@@ -0,0 +1,8 @@
+let weather = confirm("Indique como está el clima (OK:soleado/CANCEL:lluvioso): ")
+console.log(typeof weather,weather);
+let mood = confirm("Indique como esta su estado de ánimo (OK:soleado/CANCEL:lluvioso): ")
+console.log(typeof mood,mood);
+
+if(weather == true){
+
+}
